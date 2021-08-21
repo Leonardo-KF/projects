a37477d5012eaf48dbf67324e9d9ef932da55a34
leonardo <lkf352@gmail.com>
2021-08-21 15:06:24 -0300

    v1.0

---
+++ aula003.js
@@ -0,0 +1,20 @@
+let msg1 = 'Olá';
+let msg2 = ', Tudo Bem?';
+console.log(`${(msg1+msg2).toUpperCase()}`);
+// para toda frase maiuscula .toUpperCase()
+// para toda a frase minuscula .toLowerCase()
+
+
+// fatiamento
+let nome = [('leonardo'), ('gustavo')];
+//console.log(nome.slice(0,1)); // 0 é onde começa e 1 onde o corte é feito
+//console.log(nome.slice(2,-2))// subtrai os caracteres de traz pra frente
+//console.log(nome[0].toUpperCase()+nome.substr(1));
+console.log(nome[0])
+
+
+let nome = 'Amanda';
+console.log(nome.slice(0,3) + nome[5].toUpperCase());
+
+
+// funções são rotinas no meu programa
