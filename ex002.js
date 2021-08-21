7b672db76c08d69e72ac1502f70a3c380d58c01a
leonardo <lkf352@gmail.com>
2021-08-21 15:07:17 -0300

    v1.0

---
+++ ex002.js
@@ -0,0 +1,4 @@
+console.log('Nome: Leonardo Keiser Fleck')
+console.log('Endereço: Frei Caneca, 370')
+console.log('Cep: 95400-000')
+console.log('Celular: (54)996782021')
---
+++ ex004.js
@@ -0,0 +1,3 @@
+console.log('ALUNO(A) NOTA')
+console.log('='*15)
+console.log(`${Aline^10}`)
