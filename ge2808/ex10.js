867f91b591e9ded75475fe27f500d0759912040c
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-08-29 16:19:28 -0300

    Create ex10.js

---
+++ ge2808/ex10.js
@@ -0,0 +1,38 @@
+const prompt = require("prompt-sync")();
+console.log('Calculadora de salarios da empresa "xpto"!');
+let now = new Date();
+while (true) {
+  var anoini = parseInt(prompt("Digite o ano em que iniciou na empresa: "));
+  if (anoini % 1 == 0 && anoini < now.getFullYear()) {
+    break;
+  } else {
+    console.log(
+      "Você digitou uma opção invalida! Por favor digite novamente. "
+    );
+  }
+}
+while (true) {
+  var salario = parseFloat(prompt("Digite o seu salario inicial: "));
+  if (isNaN(salario)) {
+    console.log("Você digitou uma opção invalida! Por favor digite novamente.");
+  } else {
+    break;
+  }
+}
+var percent = 1.015;
+var anostrabalhados = now.getFullYear() - anoini;
+anostrabalhados -= 1;
+salario = salario * percent;
+var anosdeaument = 1;
+while (true) {
+  console.log(
+    `O seu salario após o ${anosdeaument}º ano foi: R$${salario.toFixed(2)}`
+  );
+  percent = (percent - 1) * 2 + 1;
+  salario = salario * percent;
+  anostrabalhados -= 1;
+  anosdeaument += 1;
+  if (anostrabalhados == 0) {
+    break;
+  }
+}
