46e72149667a1166ae485f0870e0b1c217c131bd
Leonardo Keiser Fleck <lkf352@gmail.com>
2021-11-16 01:24:54 -0300

    update

---
+++ dom/index.js
@@ -0,0 +1,7 @@
+const titulo = document.getElementById("titulo");
+const imagem = document.getElementById("imagem");
+const texto = document.getElementById("texto");
+
+titulo.innerHTML = "No Coffe, No Code"; // usamos a função innerHTML para adicionarmos o novo titulo dentro da nossa tag h1
+imagem.src =
+  "https://img2.gratispng.com/20180711/byi/kisspng-coffee-cup-computer-icons-programmer-clip-art-developer-icon-5b4695180dcdb4.9918957115313523440566.jpg";
